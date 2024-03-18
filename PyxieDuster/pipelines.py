b830df05cf1c87121c5096ea2c09c62c0bae99f4
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-03-18 16:48:12 -0500

    Initial commit

---
+++ PyxieDuster/pipelines.py
@@ -0,0 +1,163 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+
+class PyxiedusterPipeline:
+    def process_item(self, item, spider):
+
+        adapter = ItemAdapter(item)
+
+        # Strip whitespace
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != "description":
+                value = adapter.get(field_name)
+                adapter[field_name] = value[0].strip()
+
+        # Price
+        price_keys = ["price", "price_excl_tax", "price_incl_tax", "tax"]
+        for price_key in price_keys:
+            value = adapter.get(price_key)
+            value = value.replace("£", "")
+            adapter[price_key] = float(value)
+
+        # Extract number of books
+        avail_string = adapter.get("availability")
+        split_string = avail_string.split("(")
+        if len(split_string) < 2:
+            adapter["availability"] = 0
+        else:
+            avail_array = split_string[1].split(" ")
+            adapter["availability"] = int(avail_array[0])
+
+        # Convert reviews to int
+        num_reviews = adapter.get("num_of_reviews")
+        adapter["num_of_reviews"] = int(num_reviews)
+
+        # Convert stars to int
+        stars_string = adapter.get("stars")
+        split_stars = stars_string.split(" ")
+        stars_value = split_stars[1].lower()
+        if stars_value == "zero":
+            adapter["stars"] = 0
+        elif stars_value == "one":
+            adapter["stars"] = 1
+        elif stars_value == "two":
+            adapter["stars"] = 2
+        elif stars_value == "three":
+            adapter["stars"] = 3
+        elif stars_value == "four":
+            adapter["stars"] = 4
+        elif stars_value == "five":
+            adapter["stars"] = 5
+
+        return item
+
+
+class SaveToMySQLPipeline:
+
+    def __init__(self):
+        # * Add MySQL database: password(if set), and project db name
+        self.conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=os.getenv("PASSWORD"),
+            database=os.getenv("DATABASE_NAME"),
+        )
+
+        # Create cursor to execute commands
+        self.cur = self.conn.cursor()
+
+        # Create table if none exists
+        self.cur.execute(
+            """
+        CREATE TABLE IF NOT EXISTS books(
+            id int NOT NULL auto_increment, 
+            url VARCHAR(255),
+            title text,
+            upc VARCHAR(255),
+            product_type VARCHAR(255),
+            price_excl_tax DECIMAL,
+            price_incl_tax DECIMAL,
+            tax DECIMAL,
+            price DECIMAL,
+            availability INTEGER,
+            num_of_reviews INTEGER,
+            stars INTEGER,
+            category VARCHAR(255),
+            description text,
+            PRIMARY KEY (id)
+        )
+        """
+        )
+
+    def process_item(self, item, spider):
+
+        # Define insert statement
+        self.cur.execute(
+            """ insert into books (
+            url, 
+            title, 
+            upc, 
+            product_type, 
+            price_excl_tax,
+            price_incl_tax,
+            tax,
+            price,
+            availability,
+            num_of_reviews,
+            stars,
+            category,
+            description
+            ) values (
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s
+                )""",
+            (
+                item["url"],
+                item["title"],
+                item["upc"],
+                item["product_type"],
+                item["price_excl_tax"],
+                item["price_incl_tax"],
+                item["tax"],
+                item["price"],
+                item["availability"],
+                item["num_of_reviews"],
+                item["stars"],
+                item["category"],
+                str(item["description"][0]),
+            ),
+        )
+
+        # Execute insert of data into database
+        self.conn.commit()
+        return item
+
+    def close_spider(self, spider):
+
+        # Close cursor & connection to database
+        self.cur.close()
+        self.conn.close()
---
+++ PyxieDuster/spiders/pyxiespider.py
@@ -0,0 +1,89 @@
+from urllib.parse import urlencode
+import scrapy
+from PyxieDuster.items import PyxieItem
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+API_KEY = os.getenv("SCRAPEOPS_API_KEY")
+
+
+def get_proxy_url(url):
+    payload = {"api_key": API_KEY, "url": url}
+    proxy_url = "https://proxy.scrapeops.io/v1/?" + urlencode(payload)
+    return proxy_url
+
+
+class PyxiespiderSpider(scrapy.Spider):
+    name = "pyxiespider"
+    allowed_domains = ["books.toscrape.com", "proxy.scrapeops.io"]
+    start_urls = ["https://books.toscrape.com"]
+
+    custom_settings = {
+        "FEEDS": {
+            "data.json": {"format": "json", "overwrite": True},
+        }
+    }
+
+    def start_requests(self):
+        yield scrapy.Request(url=self.start_urls[0], callback=self.parse)
+
+    def parse(self, response):
+        books = response.css("article.product_pod")
+
+        # Loop through books on each page
+        for book in books:
+            relative_url = book.css("h3 a ::attr(href)").get()
+
+            if "catalogue/" in relative_url:
+                book_url = "https://books.toscrape.com/" + relative_url
+            else:
+                book_url = "https://books.toscrape.com/catalogue/" + relative_url
+
+            yield response.follow(
+                url=book_url,
+                callback=self.parse_book_page,
+            )
+
+        # Scrape through pages until the last page
+        next_page = response.css("li.next a ::attr(href)").get()
+
+        if next_page is not None:
+            if "catalogue/" in next_page:
+                next_page_url = "https://books.toscrape.com/" + next_page
+            else:
+                next_page_url = "https://books.toscrape.com/catalogue/" + next_page
+            yield response.follow(
+                url=next_page_url,
+                callback=self.parse,
+            )
+
+    def parse_book_page(self, response):
+        table_rows = response.css("table tr")
+        pyxie_item = PyxieItem()
+
+        # Scrape data of each book page
+
+        pyxie_item["url"] = (response.url,)
+        pyxie_item["title"] = (response.css(".product_main h1::text").get(),)
+        pyxie_item["upc"] = (table_rows[0].css("td ::text").get(),)
+        pyxie_item["product_type"] = (table_rows[1].css("td ::text").get(),)
+        pyxie_item["price_excl_tax"] = (table_rows[2].css("td ::text").get(),)
+        pyxie_item["price_incl_tax"] = (table_rows[3].css("td ::text").get(),)
+        pyxie_item["tax"] = (table_rows[4].css("td ::text").get(),)
+        pyxie_item["availability"] = (table_rows[5].css("td ::text").get(),)
+        pyxie_item["num_of_reviews"] = (table_rows[6].css("td ::text").get(),)
+        pyxie_item["stars"] = (response.css("p.star-rating").attrib["class"],)
+        pyxie_item["category"] = (
+            response.xpath(
+                "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
+            ).get(),
+        )
+        pyxie_item["description"] = (
+            response.xpath(
+                "//div[@id='product_description']/following-sibling::p/text()"
+            ).get(),
+        )
+        pyxie_item["price"] = (response.css("p.price_color ::text").get(),)
+
+        yield pyxie_item
